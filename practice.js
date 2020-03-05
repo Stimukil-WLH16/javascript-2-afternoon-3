08221302022eebdd1559a1a0060f4ee4038af50b
unknown <jeff_cox76@hotmail.com>
2020-03-05 16:34:40 -0700

    callbacks done

--- practice.js
+++ practice.js
@@ -28,6 +28,9 @@
 */
 
 // Code Here 
+function first(array, callback){
+  callback(array[0])
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -48,6 +51,10 @@ first(names, function(firstName){
 */
 
 //Code Here
+function last(array, callback){
+  callback(array[array.length - 1])
+}
+
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -67,6 +74,10 @@ last(names, function(lastName){
 
 //Code Here
 
+function multiply(num1, num2, callback){
+  callback(num1 * num2)
+}
+
 // Do not edit the code below.
 multiply(4, 3, function(answer){
   console.log('The answer is ' + answer); //should console.log 12
@@ -85,6 +96,15 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
+function contains(array, name, callback){
+  for(let i = 0; i <= array.length; i++){
+    if(array[i] === name){
+      callback(true)
+    }else{
+      callback(false)
+    }
+  }
+}
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -106,6 +126,17 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
+function uniq(array, callback){
+  for(let i = 0; i < array.length; i++){
+    for(let j = i + 1; j < array.length; j++){
+      if(array[i] === array[j]) {
+          array.splice(j, 1)
+        }         
+      }
+    }
+    callback(array)
+  }
+
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
@@ -123,6 +154,13 @@ uniq(names, function(uniqArr){
 */
 
 //Code Here 
+function each(array, callback){
+  for(let i = 0; i < array.length; i++){
+    
+    callback(array[i], i)
+  }
+}
+
 
 // Do not edit the code below.
 each(names, function(item, indice){
@@ -135,11 +173,19 @@ each(names, function(item, indice){
 ////////// PROBLEM 7 //////////
 
 /*
-  Write a function called getUserById that takes in three parameters: an array of objects (users), an id and a callback, and searches for the user with a matching id.
-  When the correct user object is found, invoke the callback with the user object as an argument.
+  Write a function called getUserById that takes in three parameters: an array of objects (users), an id and a callback, and searches 
+  for the user with a matching id. When the correct user object is found, invoke the callback with the user object as an argument.
 */
 
 // Code here
+function getUserById(users, id, callback){
+  for(let i = 0; i < users.length; i++){
+    if(users[i].id === id){
+      callback(users[i])
+    }
+  }
+}
+
 
 // Do not edit the code below.
 var users = [
